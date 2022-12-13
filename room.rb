da7c9bf22b910a0319ba9898aca914c23f63f243
Steve Gossip <stephengossip@gmail.com>
2022-12-13 00:29:28 +0000

    tests passing for a room adding and removing guests and songs from playlists.

--- room.rb
+++ room.rb
@@ -1,12 +1,37 @@
 class Room
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :guest_list, :playlist
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @guest_list = []
     @playlist = []
   end
 
   def song_count
     return @playlist.length
   end
+
+  def add_song(song)
+    @playlist.append(song)
+  end
+
+  def remove_song(song)
+    @playlist.delete(song)
+  end
+
+  def guest_count
+    return @guest_list.length
+  end
+
+  def add_guest(guest)
+    @guest_list.append(guest)
+  end
+
+  def remove_guest(guest)
+    @guest_list.delete(guest)
+    # for g in @guest_list
+    #   if g.name == guest.name
+    #
+    #   end
+    end
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,10 +1,16 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../room')
+require_relative('../guest')
+require_relative('../song')
 
 class TestRoom < MiniTest::Test
   def setup
     @room = Room.new("Rock Room", 5)
+    @guest1 = Guest.new("Steve")
+    @guest2 = Guest.new("Roisin")
+    @song1 = Song.new("Dream On", "Rock")
+    @song2 = Song.new("Song 2", "Rock")
   end
 
   def test_room_has_a_name
@@ -17,5 +23,35 @@ class TestRoom < MiniTest::Test
 
   def test_room_has_an_empty_playlist
     assert_equal(0, @room.song_count)
+  end
+
+  def test_a_room_can_accept_guests
+    @room.add_guest(@guest1)
+    assert_equal(1, @room.guest_count)
+    assert_equal("Steve", @room.guest_list[0].name)
+  end
+
+  def test_remove_guest_from_a_room
+    @room.add_guest(@guest1)
+    @room.add_guest(@guest2)
+    assert_equal(2, @room.guest_count)
+    @room.remove_guest(@guest1)
+    assert_equal(1, @room.guest_count)
+    assert_equal("Roisin", @room.guest_list[0].name)
+  end
+
+  def test_add_song_to_playlist
+    @room.add_song(@song1)
+    assert_equal("Dream On", @room.playlist[0].name)
+    assert_equal(1, @room.song_count)
+  end
+
+  def test_remove_song_from_playlist
+    @room.add_song(@song1)
+    @room.add_song(@song2)
+    assert_equal(2, @room.song_count)
+    @room.remove_song(@song1)
+    assert_equal(1, @room.song_count)
+    assert_equal("Song 2", @room.playlist[0].name)
   end
 end
